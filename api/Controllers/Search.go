20bfe581003d053c0de6859d999019bcbf86afd0
Florian <mr.leroyflorian@gmail.com>
2021-07-08 14:11:29 +0200

    feat : update delete + fix search request

--- api/Controllers/Search.go
+++ api/Controllers/Search.go
@@ -6,34 +6,37 @@ import (
 	"github.com/MT2022_PROJ03/Database"
 	"github.com/MT2022_PROJ03/Helpers"
 	"github.com/gin-gonic/gin"
-	"io"
 	"log"
 	"net/http"
 )
 
 func Search(c *gin.Context) {
 	var buf bytes.Buffer
-
-	//var value = c.Params.ByName("value")
+	var value = c.Params.ByName("value")
 
 	client, _ := Database.GetESClient()
 
-	const value = "lire"
-	var query = Helpers.MultiSearchQuery(value)
+	var fields []string
+
+	//todo passé les element du models
+	fieldResult := append(fields, "abstract", "title")
+
+	var query = Helpers.MultiSearchQuery(fieldResult, value)
 
 	if err := json.NewEncoder(&buf).Encode(&query); err != nil {
 		log.Fatalf("Error encoding query: %s", err)
 	}
-	log.Println(&buf)
-	var test io.Reader
-	res, err := client.Msearch(
-		test,
-		client.Msearch.WithPretty(),
+
+	res, err := client.Search(
+		client.Search.WithBody(&buf),
+		client.Search.WithTrackTotalHits(true),
+		client.Search.WithPretty(),
 	)
 
 	if err != nil {
 		log.Fatalf("ERROR: %s", err)
 	}
+
 	var r map[string]interface{}
 	err = json.NewDecoder(res.Body).Decode(&r)
 
--- api/Helpers/Query.go
+++ api/Helpers/Query.go
@@ -9,25 +9,6 @@ func SearchQuery(field string, value string) map[string]interface{} {
 			},
 		},
 	}
-
-	return query
-}
-
-func MultiSearchQuery(value string) map[string]interface{} {
-
-	match := []interface{}{
-		map[string]interface{}{"match": map[string]string{"abstract": value}},
-		map[string]interface{}{"match": map[string]string{"title": value}},
-	}
-
-	query := map[string]interface{}{
-		"query": map[string]interface{}{
-			"bool": map[string]interface{}{
-				"must": match,
-			},
-		},
-	}
-
 	return query
 }
 
